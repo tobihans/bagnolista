d02ebef62342d60a5ab3885f09fb43b6f86b9389
Hans Tognon <learningway403@gmail.com>
2022-03-03 16:22:49 +0100

    fix: set default value for required prop in label component

--- resources/views/components/label.blade.php
+++ resources/views/components/label.blade.php
@@ -1,4 +1,4 @@
-@props(['value', 'required'])
+@props(['value', 'required' => false])
 
 <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
     {{ $value ?? $slot }}
